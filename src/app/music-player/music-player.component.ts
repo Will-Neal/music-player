747eeb5b5fd3dacb5ed196cd5da261ab5ce95f4c
Will Neal <willwneal@gmail.com>
2024-04-27 17:36:38 -0400

    Most basic controls

--- src/app/music-player/music-player.component.ts
+++ src/app/music-player/music-player.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {resolve} from "@angular/compiler-cli";
 
 @Component({
   selector: 'app-music-player',
@@ -8,15 +9,56 @@ import {Component, OnInit} from '@angular/core';
   styleUrl: './music-player.component.scss'
 })
 export class MusicPlayerComponent implements OnInit{
+  tracksPlaying: number = 0;
+  currentTrack: HTMLAudioElement = new Audio();
+  sngIndx: number = 0;
+  soundSrcs: string[] = ['assets/track-files/9th Scientist - I Love Hip Hop.mp3', 'assets/track-files/Doctor P - Sweet Shop.mp3', 'assets/track-files/Dub Terminator - Native people (dubstep mix).mp3', 'assets/track-files/Nul Tiel Records - Sometimes.mp3', 'assets/track-files/Shaolin Dub - Kick Dis.mp3'];
+  currentTrackPath: string = this.soundSrcs[this.sngIndx];
 
-  ngOnInit() {
-    this.playAudio()
+  ngOnInit(): void {
+    this.currentTrack.src = this.soundSrcs[0];
+    this.currentTrack.load();
   }
 
-  playAudio(){
-    let audio = new Audio();
-    audio.src = 'assets/track-files/9th Scientist - I Love Hip Hop.mp3';
-    audio.load();
-    audio.play();
+  playAudio(): void{
+      this.currentTrack.play();
+  }
+
+  pauseAudio(): void {
+    this.currentTrack.pause()
+  }
+
+  playNext(): void {
+    this.currentTrack.pause();
+    if(this.sngIndx < this.soundSrcs.length - 1){
+      this.sngIndx++
+      this.currentTrackPath = this.soundSrcs[this.sngIndx];
+      this.currentTrack.src = this.currentTrackPath;
+      this.currentTrack.load();
+      this.playAudio();
+    } else {
+      console.log('No more songs');
+    }
+  }
+
+  playPrev(): void {
+    this.currentTrack.pause();
+    if(this.currentTrack.currentTime > 5) {
+      this.currentTrack.load();
+      this.currentTrack.play();
+    } else if (this.sngIndx > 0) {
+      this.sngIndx--;
+      this.currentTrackPath = this.soundSrcs[this.sngIndx];
+      this.currentTrack.src = this.currentTrackPath;
+      this.currentTrack.load();
+      this.playAudio();
+    }
+     else {
+      console.log('No Previous Tracks');
+    }
+  }
+
+  checkTime(): void {
+    console.log(this.currentTrack.currentTime)
   }
 }
